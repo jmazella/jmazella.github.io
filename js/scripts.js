3ef53abcb43968514b85936605f5db2cad412162
jmazella <jmazella@mitre.org>
2022-09-06 22:02:53 -0400

    Use cases page improvements, more images

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				lightbrown: '#F2D6C9',
+				darkbrown: '#A95131',
+				darkgrey: '#383838',
+				lightgrey: '#D9D9D9',
+				medgrey: '#9F9F9F',
+				backgroundgrey: '#F9F5F5',
+				darkred: '#98391E',
+				gold: '#D69B41',
+			},
+			textUnderlineOffset: {
+				6: '6px',
+			},
+			fontFamily: {
+				sans: ['Satoshi-Regular', 'sans-serif']
+			}
+		}
+	}
+}
