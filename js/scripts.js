835700791751b75dd1c9258ee27e549145ec3857
Margaret Cook <mkcook@mitre.org>
2022-09-22 12:39:17 -0400

    Update form - validation, build out emailjs

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,16 @@ function closemenu(){
 	document.querySelector('#open-menu-button-div').classList.remove('hidden');
 	document.querySelector('#mobile-menu').classList.add('hidden');
 }
+
+function submitform(formName){
+	event.preventDefault();
+	//generate a five digit number for the contact_number variable
+	formName.contact_number.value = Math.random() * 10000 | 0;
+	//these IDs from the previous steps
+	emailjs.sendForm('service_go5wfit', 'template_o6r4ibd', formName,'ZSEI8dIJhVsk2B3Ji')
+		.then(function(){
+			console.log('SUCCESS!');
+		}, function(error){
+			console.log('FAILED...', error);
+		});
+}
