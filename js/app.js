8b2292ba8058403c12d46a592f68565aa6e952f0
Margaret Cook <mkcook@mitre.org>
2022-09-27 13:53:38 -0400

    committees templates

--- js/app.js
+++ js/app.js
@@ -59,6 +59,62 @@ const leaders = Vue.createApp({
     }
 })
 
+const steeringCommittee = Vue.createApp({
+    template:'<ul v-for="item in commMembers"><p tabindex="2">{{item.name}} ({{item.company}})</p></ul>',
+    data(){
+        return{
+            commMembers:[
+                {
+                    name:'Dr. Steve Bratt, PhD',
+                    company:'MITRE',
+                },
+                {
+                    name:'Dr. Charles Jaffee, MD',
+                    company:'HL7',
+                },
+            ]
+        }
+    }
+})
+
+const operatingCommittee = Vue.createApp({
+    template:'<div class="flex"><div class="w-full md:w-1/2 px-4"><ul v-for="item in commMembers.slice(0, ((commMembers.length)/2))"><p tabindex="2">{{item.name}}</p></ul></div> <div class="w-full md:w-1/2 hidden lg:block"><ul v-for="item in commMembers.slice(((commMembers.length)/2), commMembers.length)"><p tabindex="2">{{item.name}}</p></ul><div><div>',
+    data(){
+        return{
+            commMembers:[
+                {name:'Alliance for Clinical Trials in Oncology'},
+                {name:'American Association of Physicists in Medicine (AAPM)'},
+                {name:'American Cancer Society - Cancer Action Network (ACS CAN)'},
+                {name:'American Society of Clinical Oncology'},
+                {name:'American Society for Radiation Oncology (ASTRO) Amgen'},
+                {name:'Centers for Disease Control and Prevention (CDC)'},
+                {name:'Center for International Blood and Marrow Transplant Research (CIBMTR)'},
+                {name:'Canadian Organization of Medical Physicists (COMP)'},
+                {name:'Evernorth'},
+                {name:'Food and Drug Administration (FDA)'},
+                {name:'Health Level Seven (HL7)'},
+                {name:'Learning Health Community'},
+                {name:'Michigan Cancer Surveillance Program'},
+                {name:'MITRE'},
+                {name:'NRG Oncology'},
+                {name:'Oncology Nursing Society'},
+                {name:'Ontada'},
+                {name:'Pfizer'},
+                {name:'Society for Imaging Informatics in Medicine'},
+                {name:'Syntropy'},
+                {name:'Telligen'},
+                {name:'Tempus'},
+                {name:'TrialJectory'},
+                {name:'UnitedHealthcare'},
+                {name:'University of Nebraska Medical Center'},
+                {name:'Varian'},
+            ]
+        }
+    }
+})
+
 //app.mount('#app')
 //leader.mount('#leader')
 leaders.mount('#leaders')
+steeringCommittee.mount('#steeringComm')
+operatingCommittee.mount('#oppComm')
