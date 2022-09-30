3b27271c2a0f50401a880637093cdf47da26a768
Margaret Cook <mkcook@mitre.org>
2022-09-30 15:58:18 -0400

    v-bind testing (will probably delete)

--- js/templates.js
+++ js/templates.js
@@ -114,9 +114,14 @@ const leaders = Vue.createApp({
     }
 });
 
-/*const steeringCommittee = Vue.createApp({
-	template: "<h1>Hi {{memName}}</h1>",
+/*let steeringCommittee = Vue.createApp({
+	//template: "<h1>Hi {{memName}}</h1>",
 	props: {memName: String},
+    data(){
+        return{
+            memName: "margaret"
+        }
+    }
 	/*template:`<ul v-for="item in commMembers">
                 <p tabindex="2">{{item.name}} ({{item.company}})</p>
             </ul>`,
