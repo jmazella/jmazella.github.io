ccf6d31c696e249b665466d4172148069a2a5a63
jmazella <jmazella@mitre.org>
2022-10-26 17:15:26 -0400

    Contact form

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,13 @@ function closemenu(){
 	document.querySelector('#open-menu-button-div').classList.remove('hidden');
 	document.querySelector('#mobile-menu').classList.add('hidden');
 }
+
+function submitContactForm(form){
+	event.preventDefault();
+	emailjs.sendForm('service_vhw7slg', 'template_q4j4dpi', form)
+		.then(function(){
+			console.log('Form Sent');
+		}, function(error){
+			console.log('Form Send failed, error: ', error);
+		});
+}
